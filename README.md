3c22fbd8c0489b95847832d00fa9fcdedb102085
zhaojunyan9528 <18803837357@163.com>
2023-06-05 09:17:10 +0800

    docs(tinymce):js引用地址修改

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 1.npm i element-themex element-theme-chalk -D 安装依赖
 2.node_modules/.bin/et -i // 创建element变量文件
-3.node_modules/.bin/et 生成theme/font css文件
+3.node_modules/.bin/et 生成theme/font css  element-variables.scss文件
--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -18,7 +18,9 @@ import toolbar from './toolbar'
 import load from './dynamicLoadScript'
 
 // why use this cdn, detail see https://github.com/PanJiaChen/tinymce-all-in-one
-const tinymceCDN = 'https://cdn.jsdelivr.net/npm/tinymce-all-in-one@4.9.3/tinymce.min.js'
+// const tinymceCDN = 'https://cdn.jsdelivr.net/npm/tinymce-all-in-one@4.9.3/tinymce.min.js'
+
+const tinymceCDN = 'https://www.72crm.com/npm/tinymce/tinymce.min.js'
 
 export default {
   name: 'Tinymce',
